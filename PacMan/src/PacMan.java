159f2763fc00cb4c1baf651137e1ec22b5eab785
WahabGuerrilla <Wahabzadafaisal@gmail.com>
2025-02-18 21:52:52 -0600

    Created Pac-Man

--- PacMan/src/PacMan.java
+++ PacMan/src/PacMan.java
@@ -1,4 +1,7 @@
-import java.awt.*; 
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
 import javax.swing.JPanel; // game panel
 
 public class PacMan extends JPanel { 
@@ -8,6 +11,16 @@ public class PacMan extends JPanel {
     private int rowCount = 21;   // height  (Up-down)
     private int columnCount = 19; // width (Left-Right)
     private int tileSize = 32; 
+    // Pac-Man's position (starting position)
+    private int pacmanX = 9 * tileSize;
+    private int pacmanY = 17 * tileSize;
+    private int pacmanSpeed = 2; // speed per frame
+    // Pac-Man's direction (int not moving)
+    private int directionX = 0;
+    private int directionY = 0;
+    // Animation frames
+    private Image[] pacmanFrames;
+    private int frameIndex = 0;
 
     // Board size based on grid dimensions 
 
@@ -34,7 +47,7 @@ public class PacMan extends JPanel {
         "X.XX.XXX.X.XXX.XX.X",
         "X..X...........X..X",
         "XX.X.X.XXXXX.X.X.XX",
-        "X....X.......X....X",
+        "X....X... ...X....X",
         "X.XXXXXX.X.XXXXXX.X",
         "X.................X",
         "XXXXXXXXXXXXXXXXXXX"
@@ -45,10 +58,53 @@ public class PacMan extends JPanel {
     PacMan(){ 
         setPreferredSize(new Dimension(boardWidth, boardHeight)); // sets Panel to match Game Board
         setBackground(Color.BLACK); // sets Background Black
-         
-    }
-
-    // Method for game to be redrawn
+        // load animation frames
+        pacmanFrames = new Image[4];
+        pacmanFrames[0] = loadImage("pacman1.png");
+                pacmanFrames[1] = loadImage("pacman2.png");
+                pacmanFrames[2] = loadImage("pacman3.png");
+                pacmanFrames[3] = loadImage("pacman2.png");
+        
+                // Listen for keyboard input
+                setFocusable(true);
+                addKeyListener(new KeyAdapter(){
+                    @Override
+                    public void keyPressed(KeyEvent e){
+                        int key = e.getKeyCode();
+        
+                        if (key == KeyEvent.VK_LEFT){
+                            directionX = -1;
+                            directionY = 0;
+                        }else if (key == KeyEvent.VK_RIGHT){
+                            directionX = 1;
+                            directionY = 0;
+                        }else if (key == KeyEvent.VK_UP){
+                            directionX = 0;
+                            directionY = -1;
+                        }else if (key == KeyEvent.VK_DOWN){
+                            directionX = 0;
+                            directionY = 1;
+                        }
+                    }
+                });
+        
+                // GAME LOOP using timer 
+                Timer timer = new Timer(100, new ActionListener(){
+                    @Override
+                    public void actionPerformed(ActionEvent e){
+                        updatePacMan();
+                    }
+        
+                });
+                timer.start(); //
+                 
+            }
+        
+            private Image loadImage(String fileName){
+                return new ImageIcon(getClass().getResource(fileName)).getImage();
+            }
+        
+            // Method for game to be redrawn
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g); // clears previous frame 
@@ -68,6 +124,24 @@ public class PacMan extends JPanel {
                 }
              }
         }
+        // draw pacman
+        if (pacmanFrames[frameIndex] != null){
+            g.drawImage(pacmanFrames[frameIndex], pacmanX,pacmanY,tileSize,tileSize,this);
+        } else{
+            System.out.println("Pac-Man Image ERROR");
+        }
+    }
+    public void updatePacMan(){
+        // Move Pac-Man Based on direction
+        pacmanX += directionX * pacmanSpeed;
+        pacmanY += directionY * pacmanSpeed;
+
+        // Cycle through animation frames
+        frameIndex = (frameIndex + 1) % pacmanFrames.length;
+
+        // Repaint the screen wit new frame (refresh screen)
+        repaint();
+
     }
 
 
